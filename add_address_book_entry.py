89bc0241f9cccf92bde69b89bffc8b2c6af67c35
diana <diana.bedryk@gmail.com>
2022-04-21 13:59:55 -0500

    commit

--- add_address_book_entry.py
+++ add_address_book_entry.py
@@ -104,18 +104,7 @@ class AddAddressBook(unittest.TestCase):
         try: self.driver.switch_to_alert()
         except NoAlertPresentException as e: return False
         return True
-    
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally: self.accept_next_alert = True
-    
+
     def tearDown(self):
         self.driver.quit()
         self.assertEqual([], self.verificationErrors)
