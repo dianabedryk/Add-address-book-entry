3520913076d5d2a690964f924e48083b4bf09567
diana <diana.bedryk@gmail.com>
2022-04-22 13:01:58 -0500

    Add group file

--- add_address_book_entry.py
+++ add_address_book_entry.py
@@ -2,6 +2,7 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 import unittest
+from group import Group
 
 class add_address_book(unittest.TestCase):
     def setUp(self):
@@ -14,9 +15,9 @@ class add_address_book(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_add_new(wd)
-        self.fill_add_address_book_entry(wd, firstname="fh", middlename="olk", lastname="ery", nickname="plk", title="ujb", company="vbn", address="ikolm", home="olkmn", mobil="tyhju",
+        self.fill_add_address_book_entry(wd, Group(firstname="fh", middlename="olk", lastname="ery", nickname="plk", title="ujb", company="vbn", address="ikolm", home="olkmn", mobil="tyhju",
                                          work="zxcvb", fax="mnbvg", email="yujn", homepage="ikmnh", bday="12", bmonth="March", byear="1980", aday="10", amonth="February",
-                                         ayear="2016", address2="hjgikkmbf", phone2="ukiolm", notes="yujnmki")
+                                         ayear="2016", address2="hjgikkmbf", phone2="ukiolm", notes="yujnmki"))
         self.submit_add_address_book_entry(wd)
         self.return_to_home_page(wd)
         self.logout(wd)
@@ -26,11 +27,11 @@ class add_address_book(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_add_new(wd)
-        self.fill_add_address_book_entry(wd, firstname="", middlename="", lastname="", nickname="",
+        self.fill_add_address_book_entry(wd, Group(firstname="", middlename="", lastname="", nickname="",
                                          title="", company="", address="", home="", mobil="",
                                          work="", fax="", email="", homepage="", bday="",
                                          bmonth="-", byear="", aday="", amonth="-",
-                                         ayear="", address2="", phone2="", notes="")
+                                         ayear="", address2="", phone2="", notes=""))
         self.submit_add_address_book_entry(wd)
         self.return_to_home_page(wd)
         self.logout(wd)
@@ -44,71 +45,69 @@ class add_address_book(unittest.TestCase):
     def submit_add_address_book_entry(self, wd):
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
-    def fill_add_address_book_entry(self, wd, firstname, middlename, lastname, nickname, title, company, address, home,
-                                    mobil, work, fax, email, homepage, bday, bmonth, byear, aday, amonth, ayear,
-                                    address2, phone2, notes):
+    def fill_add_address_book_entry(self, wd, group):
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(firstname)
+        wd.find_element_by_name("firstname").send_keys(group.firstname)
         wd.find_element_by_name("middlename").click()
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(middlename)
+        wd.find_element_by_name("middlename").send_keys(group.middlename)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(lastname)
+        wd.find_element_by_name("lastname").send_keys(group.lastname)
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(nickname)
+        wd.find_element_by_name("nickname").send_keys(group.nickname)
         wd.find_element_by_name("title").click()
         wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(title)
+        wd.find_element_by_name("title").send_keys(group.title)
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(company)
+        wd.find_element_by_name("company").send_keys(group.company)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(address)
+        wd.find_element_by_name("address").send_keys(group.address)
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(home)
+        wd.find_element_by_name("home").send_keys(group.home)
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(mobil)
+        wd.find_element_by_name("mobile").send_keys(group.mobil)
         wd.find_element_by_name("work").click()
         wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(work)
+        wd.find_element_by_name("work").send_keys(group.work)
         wd.find_element_by_name("fax").click()
         wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(fax)
+        wd.find_element_by_name("fax").send_keys(group.fax)
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(email)
+        wd.find_element_by_name("email").send_keys(group.email)
         wd.find_element_by_name("homepage").click()
         wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(homepage)
+        wd.find_element_by_name("homepage").send_keys(group.homepage)
         wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(bday)
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(group.bday)
         wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(bmonth)
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(group.bmonth)
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(byear)
+        wd.find_element_by_name("byear").send_keys(group.byear)
         wd.find_element_by_name("aday").click()
-        Select(wd.find_element_by_name("aday")).select_by_visible_text(aday)
+        Select(wd.find_element_by_name("aday")).select_by_visible_text(group.aday)
         wd.find_element_by_name("amonth").click()
-        Select(wd.find_element_by_name("amonth")).select_by_visible_text(amonth)
+        Select(wd.find_element_by_name("amonth")).select_by_visible_text(group.amonth)
         wd.find_element_by_name("ayear").click()
         wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys(ayear)
+        wd.find_element_by_name("ayear").send_keys(group.ayear)
         wd.find_element_by_name("address2").click()
         wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(address2)
+        wd.find_element_by_name("address2").send_keys(group.address2)
         wd.find_element_by_name("phone2").click()
         wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys(phone2)
+        wd.find_element_by_name("phone2").send_keys(group.phone2)
         wd.find_element_by_name("notes").click()
         wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys(notes)
+        wd.find_element_by_name("notes").send_keys(group.notes)
 
     def open_add_new(self, wd):
         wd.find_element_by_link_text("add new").click()
---
+++ group.py
@@ -0,0 +1,27 @@
+
+class Group:
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, home,
+                                    mobil, work, fax, email, homepage, bday, bmonth, byear, aday, amonth, ayear,
+                                    address2, phone2, notes):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home = home
+        self.mobil = mobil
+        self.work = work
+        self.fax = fax
+        self.email = email
+        self.homepage = homepage
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.aday = aday
+        self.amonth = amonth
+        self.ayear = ayear
+        self.address2 = address2
+        self.phone2 = phone2
+        self.notes = notes
